d78f9d840892dd115458ef60136e79f500ff4360
“Tea <teateearu@gmail.com>
2018-02-15 21:07:55 +0100

    API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getSampleText();
+  }
+  getSampleText(){
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'/html='+this.state.html)
+      .then((response) => {
+        this.setState({text: response.data.text}, function(){
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
   render() {
     return (
       <div className="App">
